70fd17b7f9e13189699b2f4de8e2926379ecad56
cwang360 <35314533+cwang360@users.noreply.github.com>
2021-05-20 21:46:02 -0400

    Pull tree images from Google Drive

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Automating Individual Event Pages.
 - Flask app deployment details figured out (site can be easily deployed once finished)
 - Loading/using event data from database is set up; script to save dict of all school event data as pickle file, and application.py gets school event data from pickle file and sends to rendered HTML page. Event page can currently show school name, event date, state, tree names, etc. based on school name in URL.
     - Some fields are empty or inconsistent (names separated by 'and', '&', or ',')
-    - Pictures of trees: add to tree_species table?
-    - Team member pictures, bios, goal message
+    - Pictures of trees: currently getting google drive image links from tree names in dataToPickle script. 
+    - Tree more info links?
+    - Team member pictures, emails, bios, goal message
+    - city?
--- dataToPickle.py
+++ dataToPickle.py
@@ -1,6 +1,11 @@
 import sys
+
 sys.path.append('../tech-team-database')
 from DatabaseSQLOperations import TpSQL
+
+sys.path.append('../common/gdrive')
+from GoogleDriveOperations import GDrive
+
 import pickle
 
 eventData = {}
@@ -11,9 +16,29 @@ df = tpSQL.getTable('event_informationall_client')
 
 eventData = df.set_index('name').T.to_dict()
 # make any string lists (list of names, trees) into lists
+# would be better to have them already as lists in the future, more consistent and reliable
+# since some names are split by & or spaces
 for school in eventData:
     eventData[school]["host_names"] = eventData[school]["host_names"].split(', ')
     eventData[school]["tree_types"] = eventData[school]["tree_types"].split(', ')
+
+
+# Getting tree images for each tree
+drive = GDrive()
+treeImages = drive.getAllFiles("12U52jiXbTfUNbANiWXLZWeJLSCvzWfvk") # Get all tree images as a dict
+treeImages = {k.lower(): v for k, v in treeImages.items()}
+print(treeImages)
+
+for school in eventData:
+    trees = eventData[school]["tree_types"]
+    for i in range(len(trees)):
+        trees[i] = {"name": trees[i]}
+        try:
+            trees[i]["image"] =  "https://drive.google.com/uc?export=view&id=" + treeImages[trees[i]["name"].lower() + ".jpg"]
+        except: 
+            print("No image for ", trees[i]["name"])
+    print(school)
+    print(eventData[school]["tree_types"])
 
 # dump dictionary into pickle file
 with open('eventDataDict.pkl', 'wb') as handle:
