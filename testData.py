4c3d7995f10f4b5a81a5108178613986e5589a12
cwang360 <35314533+cwang360@users.noreply.github.com>
2021-07-24 00:24:13 -0400

    Upload testData for demo

---
+++ testData.py
@@ -0,0 +1,122 @@
+data = {
+    'Timber Woods High School':{
+        'name': 'Timber Woods High School',
+        'state': 'Norfolk, VA',
+        'event_date': '4/12/2022',
+        'volunteerLink': '',
+        'treeLink': '',
+        'donateTrees': 0,
+        'host_names':[
+            {
+                'name':'Jake Lorenzo',
+                'role':'Club President',
+                'email':'email1@email.com',
+                'bio':'bio here bio here bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1YsNQQypYKPlYajD2xdCOMrcWOIwxtYxM',
+                'imageOrientation': 'landscape'
+            },
+            {
+                'name':'Emma Laskos',
+                'role':'Club Vice-President',
+                'email':'email3@gmail.com',
+                'bio':'bio here bio here bio here bio here bio here  bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1pD4MEpjnWTiXZjCQQavv4pEfXiIeHxPM',
+                'imageOrientation':'portrait'
+            },
+            {
+                'name':'Teo Markler',
+                'role':'Club Secretary',
+                'email':'email2@gmail.com',
+                'bio':' bio here bio here bio here bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1iilvbJzuROfsmhaliQTJ7NkqEkaRQ9Fj',
+                'imageOrientation':'portrait'
+            }
+        ],
+        'tree_goal':225, #trees
+        'paperGoal':2.3,  #millions sheets of paper
+        'treePrice': 10,
+        'saplingHeight': '2-3 feet', # calculate externally from tree info?
+        'goalMsg':'https://www.youtube.com/watch?v=Wro-pJgTOH0',
+
+        'tree_types':[
+            {
+                'name':'Eastern Redbud',
+                'image':'https://drive.google.com/uc?export=view&id=1JF8KsUHRnNWcTTaOPQL4_vyAxXobBUFq',
+                'link':'https://www.arborday.org/trees/treeguide/treedetail.cfm?itemID=912'
+            },
+            {
+                'name':'Pin Oak',
+                'image':'https://drive.google.com/uc?export=view&id=1CWcmk-fHuXNQ727-1maHTopqt6phXdd-',
+                'link':'https://www.arborday.org/trees/treeguide/TreeDetail.cfm?ItemID=879'
+            },
+        ],
+        'sponsors':[
+            {
+                'name':'Sponsor Name',
+                'url':'',
+                'image':'https://drive.google.com/uc?export=view&id=1NDyjSb21WbyMmbggZgNOyYuhTcfywrsJ'
+            }
+        ]
+    },
+    'Timber Woods High School2':{
+        'name': 'Timber Woods High School',
+        'state': 'Norfolk, VA',
+        'event_date': '4/12/2022',
+        'volunteerLink': '',
+        'treeLink': '',
+        'donateTrees': 1,
+        'host_names':[
+            {
+                'name':'Jake Lorenzo',
+                'role':'Club President',
+                'email':'email1@email.com',
+                'bio':'bio here bio here bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1YsNQQypYKPlYajD2xdCOMrcWOIwxtYxM',
+                'imageOrientation': 'landscape'
+            },
+            {
+                'name':'Emma Laskos',
+                'role':'Club Vice-President',
+                'email':'email3@gmail.com',
+                'bio':'bio here bio here bio here bio here bio here  bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1pD4MEpjnWTiXZjCQQavv4pEfXiIeHxPM',
+                'imageOrientation':'portrait'
+            },
+            {
+                'name':'Teo Markler',
+                'role':'Club Secretary',
+                'email':'email2@gmail.com',
+                'bio':' bio here bio here bio here bio here bio here bio here bio here',
+                'image':'https://drive.google.com/uc?export=view&id=1iilvbJzuROfsmhaliQTJ7NkqEkaRQ9Fj',
+                'imageOrientation':'portrait'
+            }
+        ],
+        'tree_goal':225, #trees
+        'paperGoal':2.3,  #millions sheets of paper
+        'treePrice': 10,
+        'saplingHeight': '2-3 feet', # calculate externally from tree info?
+        'goalMsg':'https://www.youtube.com/watch?v=Wro-pJgTOH0',
+
+        'tree_types':[
+            {
+                'name':'Eastern Redbud',
+                'image':'https://drive.google.com/uc?export=view&id=1JF8KsUHRnNWcTTaOPQL4_vyAxXobBUFq',
+                'link':'https://www.arborday.org/trees/treeguide/treedetail.cfm?itemID=912'
+            },
+            {
+                'name':'Pin Oak',
+                'image':'https://drive.google.com/uc?export=view&id=1CWcmk-fHuXNQ727-1maHTopqt6phXdd-',
+                'link':'https://www.arborday.org/trees/treeguide/TreeDetail.cfm?ItemID=879'
+            },
+        ],
+        'sponsors':[
+            {
+                'name':'Sponsor Name',
+                'url':'',
+                'image':'https://drive.google.com/uc?export=view&id=1NDyjSb21WbyMmbggZgNOyYuhTcfywrsJ'
+            }
+        ]
+    },
+    
+
+}
