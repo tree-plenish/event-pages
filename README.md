b9f9b6690663c3d8ef8797c651160a97c34e1549
cwang360 <cntawng24@gmail.com>
2021-07-25 21:11:42 -0400

    Reorganize package imports and update server script

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Automating Individual Event Pages.
 - General site content/structure is pretty much finished
 - Tree images are obtained from the Google Drive folder (by tree name)
 - Loading/using event data from database is in progress
+    - `dataToPickle.py` is the server script that fetches data from the database, processes it, and dumps it into a pickle file for `application.py` to send in the relevant data to populate the HTML template based on the school name appended to the URL. 
 
 ## Deployment
 - Github actions is configured to automatically deploy the Flask app on Elastic Beanstalk whenever a new change is pushed to the main branch. See workflow/deployment progress in Github "Actions" tab. 
 - TODO: Add tests to run during workflow to prevent deployment if site has problems.
-- Demo deployed [here](http://test-environment.eba-s4xs6euy.us-east-1.elasticbeanstalk.com/Timber%20Woods%20High%20School)
+- Demo deployed [here](http://flask-env.eba-rkwfeqqr.us-east-1.elasticbeanstalk.com/Timber%20Woods%20High%20School2)
+- Current state of this repo automatically deployed [here](http://test-environment.eba-s4xs6euy.us-east-1.elasticbeanstalk.com/Timber%20Woods%20High%20School)
--- dataToPickle.py
+++ dataToPickle.py
@@ -1,18 +1,38 @@
+# NOTE: This script is not updated to the new database setup yet.
+
 import sys
-
-sys.path.append('../tech-team-database')
-from DatabaseSQLOperations import TpSQL
-
-sys.path.append('../common/gdrive')
-from GoogleDriveOperations import GDrive
-
+import importlib
+from pathlib import Path
 import pickle
+
+import datetime
+from dateutil.relativedelta import relativedelta
+
+# Add parent directory to PYTHONPATH to be able to find package.
+if __name__ == '__main__' and __package__ is None:
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[2]
+
+    sys.path.append(str(top))
+    try:
+        sys.path.remove(str(parent))
+    except ValueError:
+        pass
+
+    __package__ = '.'.join(parent.parts[len(top.parts):])
+    importlib.import_module(__package__)
+
+from tech_team_database.dependencies.DatabaseSQLOperations import TpSQL
+from common.gdrive.GoogleDriveOperations import GDrive
 
 eventData = {}
 tpSQL = TpSQL()
-# print(tpSQL.listTableNames())
-df = tpSQL.getTable('event_informationall_client')
+print(tpSQL.listTableNames())
+df = tpSQL.getTable('event')
+print(df)
 
+
+# from old database setup:
 
 eventData = df.set_index('name').T.to_dict()
 # make any string lists (list of names, trees) into lists
@@ -37,6 +57,13 @@ for school in eventData:
             trees[i]["image"] =  "https://drive.google.com/uc?export=view&id=" + treeImages[trees[i]["name"].lower() + ".jpg"]
         except: 
             print("No image for ", trees[i]["name"])
+    # Calculate event date - 1 month to get order deadline
+    # Assumes event_date is always stored as '%m/%d/%Y' in database!
+    if not 'order_deadline' in eventData[school]:
+        date = datetime.datetime.strptime(eventData[school]['event_date'], '%m/%d/%Y').date()
+        date_minus_month =  date - relativedelta(months=1)
+        eventData[school]['order_deadline'] = date_minus_month.strftime('%B %d, %Y').replace(" 0", " ")
+        eventData[school]['event_date'] = date.strftime('%B %d, %Y').replace(" 0", " ")
     # print(school)
     # print(eventData[school]["tree_types"])
 
